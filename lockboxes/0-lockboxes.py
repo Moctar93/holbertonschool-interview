51ae88035282137816bbd12377d2d9389d423c65
Moctar93 <moctartize93@gmail.com>
2025-01-19 20:14:57 +0100

    Lockboxes interview

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
 
+"""
+Module for checking if all boxes can be unlocked.
+The canUnlockAll function determines if all boxes can be unlocked
+by using the keys found in other boxes.
+"""
+
+# Rename the file to '0_lockboxes.py' and then import it
+import 0_lockboxes  # Ensure the file is named correctly without hyphen
+
+print(0_lockboxes.__doc__)
+
 def canUnlockAll(boxes):
     unlocked = set()  # Set of unlocked box indices
     unlocked.add(0)   # Start with box 0 being unlocked
-    
+
     # Iterate over the boxes that are currently unlocked
     for box_index in unlocked:
         # Go through the keys in the current box
@@ -11,7 +22,7 @@ def canUnlockAll(boxes):
             # If the key corresponds to a box that isn't unlocked yet, unlock it
             if key < len(boxes) and key not in unlocked:
                 unlocked.add(key)
-    
+
     # If the number of unlocked boxes is equal to the total number of boxes, return True
     return len(unlocked) == len(boxes)
 
